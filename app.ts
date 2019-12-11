09c108151b062540c2a3f1c93e53a83da57427ea
Alexander Fedotov <alexander.g.fedotov@kaspersky.com>
2019-12-11 22:02:58 +0300

    better compiler options and dom elent existance checks

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
 console.log('test');
+
+const article = document.querySelector("article");
+
+if(article) {
+    article.addEventListener('click', (ev) => {
+        if(ev.target instanceof HTMLElement) {
+            let bg = ev.target.style.background;
+            if (bg === "red") ev.target.style.background = "blue";
+                else {
+                    ev.target.style.background = "red";
+            }
+        }
+    })
+}
